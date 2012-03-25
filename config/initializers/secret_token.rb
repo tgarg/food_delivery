3cec1bfa5abf781f78d691574fd3ae34466923b6
Tushar Garg <fuzorblade@gmail.com>
2012-03-25 12:26:06 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoodDelivery::Application.config.secret_token = 'bee107298529208fd0f72430078509447e6b5852cc7243bfd513b33db7300d3859900f8b7d58e1dc1ec7af00150972b84d90b59cf0338a3e494308334f7fd8a5'
