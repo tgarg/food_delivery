306f7b02ea2b5b66290bab351b329b2c77ea58a6
Tushar Garg <fuzorblade@gmail.com>
2012-03-27 22:03:33 -0400

    basic landing page skeleton created, CSS needs tweaking along with changes to footer/hero-unit

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -5,7 +5,7 @@ describe "Pages" do
 		
 		it "should have content 'Campus Dish'" do
 			visit root_path
-			respones.should have_content('Campus Dish')
+			response.should have_content('Campus Dish')
 		end
 	end
 end
