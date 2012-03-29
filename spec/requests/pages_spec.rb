618be1c7130bf6ae0c2ad9f26d48e2e32b1192e5
Tushar Garg <fuzorblade@gmail.com>
2012-03-29 11:02:08 -0400

    added header/footer partials, test for title

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -1,11 +1,24 @@
 require 'spec_helper'
 
 describe "Pages" do
+
 	describe 'Home page' do
-		
-		it "should have content 'Campus Dish'" do
+
+		it "should have the right title" do
 			visit root_path
-			response.should have_content('Duke Delivery')
+			page.should have_selector('title', :text => "Duke Delivery")
 		end
+	end
+
+	describe 'About page' do
+		
+	end
+
+	describe 'Contact page' do
+		
+	end
+
+	describe 'FAQ page' do
+		
 	end
 end
