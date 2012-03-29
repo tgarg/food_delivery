fd14ebe21f72662d6400747c1cdd159bdf6f60f4
Tushar Garg <fuzorblade@gmail.com>
2012-03-29 09:16:39 -0400

    finished first cut of landing page. tests for links need to be written, along with some cleaning for the views

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -5,7 +5,7 @@ describe "Pages" do
 		
 		it "should have content 'Campus Dish'" do
 			visit root_path
-			response.should have_content('Campus Dish')
+			response.should have_content('Duke Delivery')
 		end
 	end
 end
