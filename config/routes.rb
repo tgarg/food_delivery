1f7aaefaa22bf4efe7c0ee69baa9e040e35e89be
Tushar Garg <fuzorblade@gmail.com>
2012-03-29 22:24:08 -0400

    finished static-pgs, some inconsistency, signin/get-started not implemented

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ FoodDelivery::Application.routes.draw do
 
   root    :to => 'Pages#home'
 
+  match '/about',  		:to => 'Pages#about'
+  match '/faq',    		:to => 'Pages#faq'
+  match '/contact',		:to => 'Pages#contact'
+
 end
--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,21 +4,33 @@ describe "Pages" do
 
 	describe 'Home page' do
 
-		it "should have the right title" do
+		it "should have the title 'Duke Delivery" do
 			visit root_path
 			page.should have_selector('title', :text => "Duke Delivery")
 		end
 	end
 
 	describe 'About page' do
-		
+
+		it "should have the correct title" do
+			visit '/about'
+			page.should have_selector('title', :text => "Duke Delivery | About Us")			
+		end
 	end
 
 	describe 'Contact page' do
-		
+
+		it "should have the correct title" do
+			visit '/contact'
+			page.should have_selector('title', :text => "Duke Delivery | Contact Us")
+		end
 	end
 
 	describe 'FAQ page' do
 		
+		it "should have the correct title" do
+			visit '/faq'
+			page.should have_selector('title', :text => "Duke Delivery | FAQ")
+		end
 	end
 end
